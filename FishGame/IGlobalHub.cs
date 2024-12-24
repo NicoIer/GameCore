4011375c135677d093ad8c5e409331f419dde8b7
NicoIer <NicoIer@163.com>
2024-12-24 19:18:28 +0800

    feat: 新的登陆结构

--- FishGame/IGlobalHub.cs
+++ FishGame/IGlobalHub.cs
@@ -1,17 +1,27 @@
 using System.Threading.Tasks;
 using MagicOnion;
 
-namespace GameCore.FishGame{
+namespace GameCore.FishGame
+{
     /// <summary>
     /// 全局服务
     /// </summary>
     public interface IGlobalHub : IStreamingHub<IGlobalHub, IGlobalHubReceiver>
     {
-        ValueTask<RegisterResponse> Register(string nickName);
+        // ValueTask<RegisterResponse> Register(string nickName);
         ValueTask<StateResponse> GetState(uint userId);
-        ValueTask<Error> Login(uint userId);
-        ValueTask<Error> Logout(uint userId);
-        ValueTask Heartbeat(uint userId);
+        // ValueTask<Error> Login(uint userId);
+        // ValueTask<Error> Logout(uint userId);
+
+        ValueTask<Error> Register(string nickName, string macToken);
+
+        ValueTask<Error> Login(string macToken);
+
+        ValueTask<Error> Logout(string macToken);
+
+        // ValueTask Heartbeat(uint userId);
+
+        ValueTask Heartbeat(string macToken);
     }
 
     public interface IGlobalHubReceiver
@@ -19,5 +29,4 @@ namespace GameCore.FishGame{
         void PushTimeMilliSeconds(int milliSeconds);
         void PushMessage(string message);
     }
-
 }
--- FishGame/User.cs
+++ FishGame/User.cs
@@ -7,11 +7,16 @@ namespace FishGame
     public sealed partial class User
     {
         public int id { get; set; }
-        public uint uid { get; set; }
+        // public uint uid { get; set; }
         public string nickname { get; set; }
         public GlobalState globalState { get; set; }
 
         public GameState gameState { get; set; }
+        
+        /// <summary>
+        /// 账号对应的MAC地址
+        /// </summary>
+        public string macToken { get; set; }
 
         public float lastActionTimeSeconds { get; set; }
     }
