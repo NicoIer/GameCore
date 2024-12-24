59a725bd24a6d05675e47026be62bcfaa8705fa9
NicoIer <NicoIer@163.com>
2024-12-24 17:33:02 +0800

    Create User.cs

---
+++ FishGame/User.cs
@@ -0,0 +1,18 @@
+using GameCore.FishGame;
+using MemoryPack;
+
+namespace FishGame
+{
+    [MemoryPackable]
+    public sealed partial class User
+    {
+        public int id { get; set; }
+        public uint uid { get; set; }
+        public string nickname { get; set; }
+        public GlobalState GlobalState { get; set; }
+
+        public GameState gameState { get; set; }
+
+        public float lastActionTimeSeconds { get; set; }
+    }
+}
